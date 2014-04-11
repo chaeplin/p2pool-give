bfc959530d5fe9536f35025078c7c84e01733340
chaeplin <chaeplin@gmail.com>
2014-04-11 17:22:56 +0900

    first commit

---
+++ p2pool/bitcoin/networks.py
@@ -0,0 +1,47 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from . import data
+from p2pool.util import math, pack, jsonrpc
+from operator import *
+
+
+@defer.inlineCallbacks
+def check_genesis_block(bitcoind, genesis_block_hash):
+    try:
+        yield bitcoind.rpc_getblock(genesis_block_hash)
+    except jsonrpc.Error_for_code(-5):
+        defer.returnValue(False)
+    else:
+        defer.returnValue(True)
+
+nets = dict(
+
+    givecoin=math.Object(
+        P2P_PREFIX='d1d2d3db'.decode('hex'),
+        P2P_PORT=31415,
+        ADDRESS_VERSION=15,
+        RPC_PORT=31410,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'Givecoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),                           
+        SUBSIDY_FUNC=lambda height: 1000*100000000 >> (height + 1)//250000,
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='GIVE',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Givecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Givecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.Givecoin'), 'Givecoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1), 
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
+
+)
+for net_name, net in nets.iteritems():
+    net.NAME = net_name
---
+++ p2pool/networks.py
@@ -0,0 +1,35 @@
+from p2pool.bitcoin import networks
+from p2pool.util import math
+
+# CHAIN_LENGTH = number of shares back client keeps
+# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
+# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
+# REAL_CHAIN_LENGTH must be changed in sync with all other clients
+# changes can be done by changing one, then the other
+
+nets = dict(
+
+    givecoin=math.Object(
+        PARENT=networks.nets['givecoin'],
+        SHARE_PERIOD=15, # seconds
+        NEW_SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=30, # blocks
+        NEW_SPREAD=30, # blocks
+        IDENTIFIER='17cf94c0ae00e08f'.decode('hex'),
+        PREFIX='5559f46dfee0001f'.decode('hex'),
+        P2P_PORT=17902,
+        MIN_TARGET=1,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=17903,
+        BOOTSTRAP_ADDRS='asia01.poolhash.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-give',
+        VERSION_CHECK=lambda v: True,
+    ),
+
+)
+for net_name, net in nets.iteritems():
+    net.NAME = net_name
